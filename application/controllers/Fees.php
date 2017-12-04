5b604887170547d1daca4cf7f9a80fa641f048c3
13cse169 <13cse169@gmail.com>
2017-12-04 16:34:52 +0530

    msg

--- application/controllers/Fees.php
+++ application/controllers/Fees.php
@@ -11,17 +11,17 @@ class Fees extends MY_Controller {
 	}
 
 	function index() {
-		$this->data = array(
-			'title'=> 'Fee Category',
-			'subview'=> 'test',
-			'script'=> '',
-			'li1' => 'finance',
-			'a1' => 'finance',
-			'div1' => 'finance',
-			'li2' => 'fee',
-			'a2' => 'fee',
-			'div2' => 'fee',
-			'li3' => 'fee-category'
+		$this->data   = array(
+			'title'   => 'Fee Category',
+			'subview' => 'test',
+			'script'  => '',
+			'li1'     => 'finance',
+			'a1'      => 'finance',
+			'div1'    => 'finance',
+			'li2'     => 'fee',
+			'a2'      => 'fee',
+			'div2'    => 'fee',
+			'li3'     => 'fee-category'
 		);
 		$this->load->view('main_layout',$this->data);
 	}
